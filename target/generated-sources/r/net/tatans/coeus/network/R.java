5938eb36b35248412307e852abc2ca0c82e226c1
shengxiadeyu <750749212@qq.com>
2015-08-26 11:06:01 +0800

    Signed-off-by: shengxiadeyu <750749212@qq.com>

--- target/generated-sources/r/net/tatans/coeus/network/R.java
+++ target/generated-sources/r/net/tatans/coeus/network/R.java
@@ -17,45 +17,45 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static int activity_horizontal_margin=0x7f050000;
+        public static int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int background_login=0x7f020000;
-        public static final int background_login_div_bg=0x7f020001;
-        public static final int cat=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int icon_tatans=0x7f020004;
-        public static final int logo=0x7f020005;
+        public static int background_login=0x7f020000;
+        public static int background_login_div_bg=0x7f020001;
+        public static int cat=0x7f020002;
+        public static int ic_launcher=0x7f020003;
+        public static int icon_tatans=0x7f020004;
+        public static int logo=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int login=0x7f090005;
-        public static final int login_div=0x7f090000;
-        public static final int login_password_input=0x7f090003;
-        public static final int login_user_input=0x7f090001;
-        public static final int message=0x7f090006;
-        public static final int miniTwitter_logo=0x7f090007;
-        public static final int password_edit=0x7f090004;
-        public static final int username_edit=0x7f090002;
+        public static int action_settings=0x7f090008;
+        public static int login=0x7f090005;
+        public static int login_div=0x7f090000;
+        public static int login_password_input=0x7f090003;
+        public static int login_user_input=0x7f090001;
+        public static int message=0x7f090006;
+        public static int miniTwitter_logo=0x7f090007;
+        public static int password_edit=0x7f090004;
+        public static int username_edit=0x7f090002;
     }
     public static final class layout {
-        public static final int account_login=0x7f030000;
+        public static int account_login=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static int main=0x7f080000;
     }
     public static final class string {
-        public static final int img_logo_des=0x7f060008;
-        public static final int img_pic_des=0x7f060007;
-        public static final int label=0x7f060002;
-        public static final int login_activity_loginfail_text_pwmissing=0x7f060001;
-        public static final int login_activity_loginfail_text_pwonly=0x7f060000;
-        public static final int login_activity_newaccount_text=0x7f060009;
-        public static final int login_label_password=0x7f060004;
-        public static final int login_label_signin=0x7f060005;
-        public static final int login_label_username=0x7f060003;
-        public static final int login_message_remind=0x7f060006;
+        public static int img_logo_des=0x7f060008;
+        public static int img_pic_des=0x7f060007;
+        public static int label=0x7f060002;
+        public static int login_activity_loginfail_text_pwmissing=0x7f060001;
+        public static int login_activity_loginfail_text_pwonly=0x7f060000;
+        public static int login_activity_newaccount_text=0x7f060009;
+        public static int login_label_password=0x7f060004;
+        public static int login_label_signin=0x7f060005;
+        public static int login_label_username=0x7f060003;
+        public static int login_message_remind=0x7f060006;
     }
     public static final class style {
         /** 
@@ -70,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070001;
-        public static final int normalText=0x7f070000;
+        public static int AppBaseTheme=0x7f070001;
+        public static int normalText=0x7f070000;
     }
     public static final class xml {
-        public static final int authenticator=0x7f040000;
+        public static int authenticator=0x7f040000;
     }
 }
